b788efd7916504c979483ea0fc792d13d1b6760b
n7esports <n7esportspubgm@gmail.com>
2026-08-20 16:36:53 +0500

    Add files via upload

--- src/pages/Page1/types.ts
+++ src/pages/Page1/types.ts
@@ -1,4 +1,4 @@
-export type Page1State = 'countdown' | 'fireworks' | 'cakeAssembly' | 'blowOut' | 'balloons'
+export type Page1State = 'countdown' | 'fireworks' | 'cakeAssembly' | 'blowOut' | 'BallonsPhase'
 
 export interface Particle {
   x: number
