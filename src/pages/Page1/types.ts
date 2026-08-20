d34d77599529ebfadf3684ee4a6941d193021af0
n7esports <n7esportspubgm@gmail.com>
2026-08-20 17:13:39 +0500

    Update types.ts

--- src/pages/Page1/types.ts
+++ src/pages/Page1/types.ts
@@ -1,4 +1,4 @@
-export type Page1State = 'countdown' | 'fireworks' | 'cakeAssembly' | 'blowOut' | 'balloons'
+export type Page1State = 'countdown' | 'fireworks' | 'cakeAssembly' | 'blowOut' | 'skyOfWishes'
 
 export interface Particle {
   x: number
